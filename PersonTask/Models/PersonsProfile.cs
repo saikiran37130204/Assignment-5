8f2532fec3ee84d8e176587006d6c0df581e8d47
Kondreddy Saikiran reddy <67851495+saikiran37130204@users.noreply.github.com>
2021-05-22 09:49:05 +0530

    Add files via upload

---
+++ PersonTask/Models/PersonsProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonsProfileMVC.Models
+{
+    public class PersonsProfile
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string qualification { get; set; }
+        public string isEmployed { get; set; }
+        public int noticePeriod { get; set; }
+        public double currentCTC { get; set; }
+    }
+}
---
+++ PersonTask/Models/PersonsProfileContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonsProfileMVC.Models
+{
+    
+        public class PersonsProfileContext : DbContext
+        {
+        public PersonsProfileContext() : base()
+        {
+
+        }
+        public PersonsProfileContext(DbContextOptions options) : base(options)
+            {
+
+            }
+            public DbSet<PersonsProfile> persons { get; set; }
+
+        }
+}
