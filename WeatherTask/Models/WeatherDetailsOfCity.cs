59fc340dabb3d866bb8959366e2b0ebeb0e27db2
Kondreddy Saikiran reddy <67851495+saikiran37130204@users.noreply.github.com>
2021-05-22 10:01:22 +0530

    Add files via upload

---
+++ WeatherTask/Models/WeatherDetailsOfCity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherDetailsWebAPI.Models
+{
+    public class WeatherDetailsOfCity
+    {
+        [Key]
+        public string city { get; set; }
+        public DateTime date { get; set; }
+        public double highTemp { get; set; }
+        public double lowTemp { get; set; }
+        public string forecast { get; set; }
+    }
+}
---
+++ WeatherTask/Models/WeatherDetailsOfCityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherDetailsWebAPI.Models
+{
+    public class WeatherDetailsOfCityDbContext:DbContext
+    {
+        public WeatherDetailsOfCityDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<WeatherDetailsOfCity> weathers { get; set; }
+    }
+}
