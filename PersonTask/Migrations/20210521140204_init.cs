77dd300dd7010d3a35aba58e6958ed60c4a8d99c
Kondreddy Saikiran reddy <67851495+saikiran37130204@users.noreply.github.com>
2021-05-22 09:41:28 +0530

    Add files via upload

---
+++ PersonTask/Migrations/20210521140204_init.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PersonsProfileMVC.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "persons",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    age = table.Column<int>(type: "int", nullable: false),
+                    qualification = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    isEmployed = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    noticePeriod = table.Column<int>(type: "int", nullable: false),
+                    currentCTC = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_persons", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "persons");
+        }
+    }
+}
